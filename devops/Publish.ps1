f5298e4c06c4c34be54068b844fe45ca20d59fc5
Vinicius Jarina <viniciusjarina@gmail.com>
2020-02-28 21:05:21 -0800

    Added Symbol Package

--- devops/Publish.ps1
+++ devops/Publish.ps1
@@ -6,6 +6,12 @@ if (-Not (Test-Should-Deploy)) {
 	return
 }
 
-$nupkgFile = $PackageId +  '.nupkg'
+$nupkgFile  = $PackageId + '.nupkg'
+$snupkgFile = $PackageId + '.snupkg'
 
 & nuget push $nupkgFile -Source https://www.nuget.org/api/v2/package
+
+if (Test-Path -Path $snupkgFile) {
+	& nuget push $snupkgFile -Source https://www.nuget.org/api/v2/package
+}
+
