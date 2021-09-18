b5ae2d7478e4347700f82d46cc028c64fb1189b9
Vinicius Jarina <viniciusjarina@gmail.com>
2021-09-18 12:20:35 -0700

    Squashed `codefoco-build` commits
    * [UWP] Use UAPGameWindows game instance
    * Removed Deflate files (#5)
    * Delete stb sharp (#4)
    * [UWP] Fixed UWP game initialization
    * Fixed pdb path on NuGet
    * [NuGet] Added pdb to Nuget since VS4M doesn't support symbol server.
    * [UWP] Use Game as parameter instead of dynamically created.
    * Fixed Publish.ps1 to do not push the symbol package (is pushed implicitly)
    * Fixed Nuget pack target path.
    * Fixed UWP build
    * Fixed nuget packaging after rebase upstream MonoGame
    * Fix csproj name iOSCore -> iOS
    * Add sourcelink nuget reference
    * Added Symbol Package
    * Update submodule URL
    * Fixed UWP NuGet and Fixed CI version update
    * Update WindowsDX.nuspec during the build
    * Added `WindowsDX` to CI build. (#1)
    * Fixed NuGet spec file typo
    * Unify YAML
    * Fixed targets path.
    * Fixed build on net45
    * Use .NET 4.5
    * Fixing nuget nuspec for Android build
    * Fixed build errors MockWindow, MarshalStringToPtr
    * Update to .NET Core 3.1
    * [devops] Fixed `tvOS` target on nuspec
    * [devops] Fixed `TriggerBuild` step
    * [devops] Added `azure-devops-win.yml`
    * [devops] Fixed Artifact path.
    * [devops] Checkou submodules.
    * [devops] Added `azure-devops-macos.yml`
    * [iOS|tvOS] Fixed crash on `PlatformClear`
    * [build] Added `tvOS` support back.
    * [build] Added `Codefoco.MonoGame.Framework.nuspec` to create multi-target NuGet.
    * Use `CopyToOutputDirectory` to copy OpenAL/libSDL libraries.
    * Add `Xamarin.Mac` profile to NuGet.
    * Bumped SDL_GameControllerDB
    * Add `AllowHighDPI` in `SDLGameWindow`
    * Removed NETSTANDARD macro
    * Add AppKit/CoreGraphics implementation to Load/Save textures.
    * Revert commit bc7eac3 (was causing NRE on UWP)
    * Add `SpriteBatch.ApplyTransformation` to avoid to do matrix multiplication everytime on `SpriteBatch.Begin`
    * Add `World` and `Projection` to `SpriteBatch`, fixed `SpriteBatch` to render clockwise
    * Fixed PNG alpha on SaveAsPng on UWP
    * Fixed missing LeftShoulder/RightShoulder on iOS
    * Bump CI to Mac 10.15
    * Update MojoShader to July/2020 version
    * Updated embedded shaders to use OpenGL/OpenGLES GLSL
    * Added `SmoothEdgeOption` to BasicEffect
    * Added `DrawString` with transform
    * Added `Draw` with multiple colors
    * Re-removed compress classes
    * Bump SDL_GameControllerDB
    * Fixed minor issues after run PVS
    * Remove check on reseting GraphicsDevice on BlendState (is unusual, but was causing integration tests to fail)
    * Fix `DrawString` regression + Added Draw with sourceRect
    * Bump SDL with AppleSilicon support
    * Minor performance improvements
    * Added `GameWindow.GetDeviceDPI` and fixed Viewport size using (`Sdl.GL.GetDrawableSize`)
    * Fixed DPI aware on GL and DX (Windows)
    * Fixed Window.ScreenScale (Android, iOS, Mac)
    * Removed BinaryReaderEx
    * Revert System.Numerics dependency (it can be done using extension methods without adding a dependency to MG)
    * Fixed issue with resizable window on macOS
    * Fixed `PlatformFromStream` to multiply alpha on System.Drawing implementation
    * Fixed resize viewport update on windows
    * Removed  + `net5.0`, moved `net45` -> `net46`
    * Added MouseState.ClickCount
    * Fixed .NET 5 nuget target
    * Skipped MathF change
    * Skipped Color transformation (since we have no StbSharp)
    * Added `PrepareDrawString`
    * Bump ADO to `macos-11`

--- MonoGame.Framework/Platform/Graphics/GraphicsDevice.OpenGL.cs
+++ MonoGame.Framework/Platform/Graphics/GraphicsDevice.OpenGL.cs
@@ -116,9 +116,10 @@ namespace Microsoft.Xna.Framework.Graphics
         static readonly float[] _posFixup = new float[4];
 
         private static BufferBindingInfo[] _bufferBindingInfos;
+
         private static int _activeBufferBindingInfosCount;
-        private static bool[] _newEnabledVertexAttributes;
-        internal static readonly List<int> _enabledVertexAttributes = new List<int>();
+        private static int _newEnabledVertexAttributes;
+        private static int _enabledVertexAttributes;
         internal static bool _attribsDirty;
 
         internal FramebufferHelper framebufferHelper;
@@ -144,7 +145,7 @@ namespace Microsoft.Xna.Framework.Graphics
         {
             get
             {
-                if (_vertexShader == null && _pixelShader == null)
+                if (_vertexShader == null || _pixelShader == null)
                     throw new InvalidOperationException("There is no shader bound!");
                 if (_vertexShader == null)
                     return _pixelShader.HashKey;
@@ -154,22 +155,25 @@ namespace Microsoft.Xna.Framework.Graphics
             }
         }
 
-        internal void SetVertexAttributeArray(bool[] attrs)
+        internal void SetVertexAttributeArray(int attrs, int maxAttributes)
         {
-            for (var x = 0; x < attrs.Length; x++)
+            for (int x = 0; x < maxAttributes && attrs != _enabledVertexAttributes; x++)
             {
-                if (attrs[x] && !_enabledVertexAttributes.Contains(x))
+                int flag = 1 << x;
+
+                if ((attrs & flag) == flag && (_enabledVertexAttributes & flag) != flag )
                 {
-                    _enabledVertexAttributes.Add(x);
+                    _enabledVertexAttributes |= flag;
                     GL.EnableVertexAttribArray(x);
                     GraphicsExtensions.CheckGLError();
                 }
-                else if (!attrs[x] && _enabledVertexAttributes.Contains(x))
+                else if ((attrs & flag) != flag && (_enabledVertexAttributes & flag) == flag)
                 {
-                    _enabledVertexAttributes.Remove(x);
+                    _enabledVertexAttributes &= ~flag;
                     GL.DisableVertexAttribArray(x);
                     GraphicsExtensions.CheckGLError();
                 }
+
             }
         }
 
@@ -204,8 +208,9 @@ namespace Microsoft.Xna.Framework.Graphics
                 if (!GraphicsCapabilities.SupportsInstancing && vertexBufferBinding.InstanceFrequency > 0)
                     throw new PlatformNotSupportedException("Instanced geometry drawing requires at least OpenGL 3.2 or GLES 3.2. Try upgrading your graphics drivers.");
 
-                foreach (var element in attrInfo.Elements)
+                for (int i = 0; i < attrInfo.Elements.Count; i++)
                 {
+                    var element = attrInfo.Elements[i];
                     GL.VertexAttribPointer(element.AttributeLocation,
                         element.NumberOfElements,
                         element.VertexAttribPointerType,
@@ -230,15 +235,19 @@ namespace Microsoft.Xna.Framework.Graphics
 
             if (bindingsChanged)
             {
-                Array.Clear(_newEnabledVertexAttributes, 0, _newEnabledVertexAttributes.Length);
+                _newEnabledVertexAttributes = 0;
+
                 for (var slot = 0; slot < _vertexBuffers.Count; slot++)
                 {
-                    foreach (var element in _bufferBindingInfos[slot].AttributeInfo.Elements)
-                        _newEnabledVertexAttributes[element.AttributeLocation] = true;
+                    for (int i = 0; i < _bufferBindingInfos[slot].AttributeInfo.Elements.Count; i++)
+                    {
+                        var element = _bufferBindingInfos[slot].AttributeInfo.Elements[i];
+                        _newEnabledVertexAttributes |= 1 << element.AttributeLocation;
+                    }
                 }
                 _activeBufferBindingInfosCount = _vertexBuffers.Count;
             }
-            SetVertexAttributeArray(_newEnabledVertexAttributes);
+            SetVertexAttributeArray(_newEnabledVertexAttributes, _maxVertexBufferSlots);
         }
 
         private void PlatformSetup()
@@ -267,7 +276,7 @@ namespace Microsoft.Xna.Framework.Graphics
             GraphicsExtensions.CheckGLError();
 
             _maxVertexBufferSlots = MaxVertexAttributes;
-            _newEnabledVertexAttributes = new bool[MaxVertexAttributes];
+            _newEnabledVertexAttributes = 0;
 
 
             // try getting the context version
@@ -336,7 +345,7 @@ namespace Microsoft.Xna.Framework.Graphics
             _viewport = new Viewport(0, 0, PresentationParameters.BackBufferWidth, PresentationParameters.BackBufferHeight);
 
             // Ensure the vertex attributes are reset
-            _enabledVertexAttributes.Clear();
+            _enabledVertexAttributes = 0;
 
             // Free all the cached shader programs. 
             _programCache.Clear();
@@ -686,20 +695,17 @@ namespace Microsoft.Xna.Framework.Graphics
 #endif
                 }
 
-                if (depthInternalFormat != 0)
+                this.framebufferHelper.GenRenderbuffer(out depth);
+                this.framebufferHelper.BindRenderbuffer(depth);
+                this.framebufferHelper.RenderbufferStorageMultisample(preferredMultiSampleCount, (int)depthInternalFormat, width, height);
+                if (preferredDepthFormat == DepthFormat.Depth24Stencil8)
                 {
-                    this.framebufferHelper.GenRenderbuffer(out depth);
-                    this.framebufferHelper.BindRenderbuffer(depth);
-                    this.framebufferHelper.RenderbufferStorageMultisample(preferredMultiSampleCount, (int)depthInternalFormat, width, height);
-                    if (preferredDepthFormat == DepthFormat.Depth24Stencil8)
+                    stencil = depth;
+                    if (stencilInternalFormat != 0)
                     {
-                        stencil = depth;
-                        if (stencilInternalFormat != 0)
-                        {
-                            this.framebufferHelper.GenRenderbuffer(out stencil);
-                            this.framebufferHelper.BindRenderbuffer(stencil);
-                            this.framebufferHelper.RenderbufferStorageMultisample(preferredMultiSampleCount, (int)stencilInternalFormat, width, height);
-                        }
+                        this.framebufferHelper.GenRenderbuffer(out stencil);
+                        this.framebufferHelper.BindRenderbuffer(stencil);
+                        this.framebufferHelper.RenderbufferStorageMultisample(preferredMultiSampleCount, (int)stencilInternalFormat, width, height);
                     }
                 }
             }
@@ -1084,23 +1090,18 @@ namespace Microsoft.Xna.Framework.Graphics
 
             // Pin the buffers.
             var vbHandle = GCHandle.Alloc(vertexData, GCHandleType.Pinned);
-            try
-            {
-                // Setup the vertex declaration to point at the VB data.
-                vertexDeclaration.GraphicsDevice = this;
-                vertexDeclaration.Apply(_vertexShader, vbHandle.AddrOfPinnedObject(), ShaderProgramHash);
+            // Setup the vertex declaration to point at the VB data.
+            vertexDeclaration.GraphicsDevice = this;
+            vertexDeclaration.Apply(_vertexShader, vbHandle.AddrOfPinnedObject(), ShaderProgramHash);
 
-                //Draw
-                GL.DrawArrays(PrimitiveTypeGL(primitiveType),
-                              vertexOffset,
-                              vertexCount);
-                GraphicsExtensions.CheckGLError();
-            }
-            finally
-            {
-                // Release the handles.
-                vbHandle.Free();
-            }
+            //Draw
+            GL.DrawArrays(PrimitiveTypeGL(primitiveType),
+                            vertexOffset,
+                            vertexCount);
+            GraphicsExtensions.CheckGLError();
+
+            // Release the handles.
+            vbHandle.Free();
         }
 
         private void PlatformDrawPrimitives(PrimitiveType primitiveType, int vertexStart, int vertexCount)
@@ -1134,28 +1135,24 @@ namespace Microsoft.Xna.Framework.Graphics
             // Pin the buffers.
             var vbHandle = GCHandle.Alloc(vertexData, GCHandleType.Pinned);
             var ibHandle = GCHandle.Alloc(indexData, GCHandleType.Pinned);
-            try
-            {
-                var vertexAddr = (IntPtr)(vbHandle.AddrOfPinnedObject().ToInt64() + vertexDeclaration.VertexStride * vertexOffset);
 
-                // Setup the vertex declaration to point at the VB data.
-                vertexDeclaration.GraphicsDevice = this;
-                vertexDeclaration.Apply(_vertexShader, vertexAddr, ShaderProgramHash);
+            var vertexAddr = (IntPtr)(vbHandle.AddrOfPinnedObject().ToInt64() + vertexDeclaration.VertexStride * vertexOffset);
 
-                //Draw
-                GL.DrawElements(
-                    PrimitiveTypeGL(primitiveType),
-                    GetElementCountArray(primitiveType, primitiveCount),
-                    DrawElementsType.UnsignedShort,
-                    (IntPtr)(ibHandle.AddrOfPinnedObject().ToInt64() + (indexOffset * sizeof(short))));
-                GraphicsExtensions.CheckGLError();
-            }
-            finally
-            {
-                // Release the handles.
-                ibHandle.Free();
-                vbHandle.Free();
-            }
+            // Setup the vertex declaration to point at the VB data.
+            vertexDeclaration.GraphicsDevice = this;
+            vertexDeclaration.Apply(_vertexShader, vertexAddr, ShaderProgramHash);
+
+            //Draw
+            GL.DrawElements(
+                PrimitiveTypeGL(primitiveType),
+                GetElementCountArray(primitiveType, primitiveCount),
+                DrawElementsType.UnsignedShort,
+                (IntPtr)(ibHandle.AddrOfPinnedObject().ToInt64() + (indexOffset * sizeof(short))));
+            GraphicsExtensions.CheckGLError();
+
+            // Release the handles.
+            ibHandle.Free();
+            vbHandle.Free();
         }
 
         private void PlatformDrawUserIndexedPrimitives<T>(
@@ -1174,28 +1171,24 @@ namespace Microsoft.Xna.Framework.Graphics
             // Pin the buffers.
             var vbHandle = GCHandle.Alloc(vertexData, GCHandleType.Pinned);
             var ibHandle = GCHandle.Alloc(indexData, GCHandleType.Pinned);
-            try
-            {
-                var vertexAddr = (IntPtr)(vbHandle.AddrOfPinnedObject().ToInt64() + vertexDeclaration.VertexStride * vertexOffset);
 
-                // Setup the vertex declaration to point at the VB data.
-                vertexDeclaration.GraphicsDevice = this;
-                vertexDeclaration.Apply(_vertexShader, vertexAddr, ShaderProgramHash);
+            var vertexAddr = (IntPtr)(vbHandle.AddrOfPinnedObject().ToInt64() + vertexDeclaration.VertexStride * vertexOffset);
 
-                //Draw
-                GL.DrawElements(
-                    PrimitiveTypeGL(primitiveType),
-                    GetElementCountArray(primitiveType, primitiveCount),
-                    DrawElementsType.UnsignedInt,
-                    (IntPtr)(ibHandle.AddrOfPinnedObject().ToInt64() + (indexOffset * sizeof(int))));
-                GraphicsExtensions.CheckGLError();
-            }
-            finally
-            {
-                // Release the handles.
-                ibHandle.Free();
-                vbHandle.Free();
-            }
+            // Setup the vertex declaration to point at the VB data.
+            vertexDeclaration.GraphicsDevice = this;
+            vertexDeclaration.Apply(_vertexShader, vertexAddr, ShaderProgramHash);
+
+            //Draw
+            GL.DrawElements(
+                PrimitiveTypeGL(primitiveType),
+                GetElementCountArray(primitiveType, primitiveCount),
+                DrawElementsType.UnsignedInt,
+                (IntPtr)(ibHandle.AddrOfPinnedObject().ToInt64() + (indexOffset * sizeof(int))));
+            GraphicsExtensions.CheckGLError();
+
+            // Release the handles.
+            ibHandle.Free();
+            vbHandle.Free();
         }
 
         private void PlatformDrawInstancedPrimitives(PrimitiveType primitiveType, int baseVertex, int startIndex, int primitiveCount, int baseInstance, int instanceCount)
