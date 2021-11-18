7ba2aa1e6a4220bfa65b508c15f58ebf3d313e11
Vinicius Jarina <viniciusjarina@gmail.com>
2021-11-18 00:17:22 -0800

    Squashed `codefoco-build` commits
    * [UWP] Use UAPGameWindows game instance
    * Removed Deflate files (#5)
    * Delete stb sharp (#4)
    * [UWP] Fixed UWP game initialization
    * Fixed pdb path on NuGet
    * [NuGet] Added pdb to Nuget since VS4M doesn't support symbol server.
    * [UWP] Use Game as parameter instead of dynamically created.
    * Fixed Publish.ps1 to do not push the symbol package (is pushed implicitly)
    * Fixed Nuget pack target path.
    * Fixed UWP build
    * Fixed nuget packaging after rebase upstream MonoGame
    * Fix csproj name iOSCore -> iOS
    * Add sourcelink nuget reference
    * Added Symbol Package
    * Update submodule URL
    * Fixed UWP NuGet and Fixed CI version update
    * Update WindowsDX.nuspec during the build
    * Added `WindowsDX` to CI build. (#1)
    * Fixed NuGet spec file typo
    * Unify YAML
    * Fixed targets path.
    * Fixed build on net45
    * Use .NET 4.5
    * Fixing nuget nuspec for Android build
    * Fixed build errors MockWindow, MarshalStringToPtr
    * Update to .NET Core 3.1
    * [devops] Fixed `tvOS` target on nuspec
    * [devops] Fixed `TriggerBuild` step
    * [devops] Added `azure-devops-win.yml`
    * [devops] Fixed Artifact path.
    * [devops] Checkou submodules.
    * [devops] Added `azure-devops-macos.yml`
    * [iOS|tvOS] Fixed crash on `PlatformClear`
    * [build] Added `tvOS` support back.
    * [build] Added `Codefoco.MonoGame.Framework.nuspec` to create multi-target NuGet.
    * Use `CopyToOutputDirectory` to copy OpenAL/libSDL libraries.
    * Add `Xamarin.Mac` profile to NuGet.
    * Bumped SDL_GameControllerDB
    * Add `AllowHighDPI` in `SDLGameWindow`
    * Removed NETSTANDARD macro
    * Add AppKit/CoreGraphics implementation to Load/Save textures.
    * Revert commit bc7eac3 (was causing NRE on UWP)
    * Add `SpriteBatch.ApplyTransformation` to avoid to do matrix multiplication everytime on `SpriteBatch.Begin`
    * Add `World` and `Projection` to `SpriteBatch`, fixed `SpriteBatch` to render clockwise
    * Fixed PNG alpha on SaveAsPng on UWP
    * Fixed missing LeftShoulder/RightShoulder on iOS
    * Bump CI to Mac 10.15
    * Update MojoShader to July/2020 version
    * Updated embedded shaders to use OpenGL/OpenGLES GLSL
    * Added `SmoothEdgeOption` to BasicEffect
    * Added `DrawString` with transform
    * Added `Draw` with multiple colors
    * Re-removed compress classes
    * Bump SDL_GameControllerDB
    * Fixed minor issues after run PVS
    * Remove check on reseting GraphicsDevice on BlendState (is unusual, but was causing integration tests to fail)
    * Fix `DrawString` regression + Added Draw with sourceRect
    * Bump SDL with AppleSilicon support
    * Minor performance improvements
    * Added `GameWindow.GetDeviceDPI` and fixed Viewport size using (`Sdl.GL.GetDrawableSize`)
    * Fixed DPI aware on GL and DX (Windows)
    * Fixed Window.ScreenScale (Android, iOS, Mac)
    * Removed BinaryReaderEx
    * Revert System.Numerics dependency (it can be done using extension methods without adding a dependency to MG)
    * Fixed issue with resizable window on macOS
    * Fixed `PlatformFromStream` to multiply alpha on System.Drawing implementation
    * Fixed resize viewport update on windows
    * Removed  + `net5.0`, moved `net45` -> `net46`
    * Added MouseState.ClickCount
    * Fixed .NET 5 nuget target
    * Skipped MathF change
    * Skipped Color transformation (since we have no StbSharp)
    * Added `PrepareDrawString`
    * Bump ADO to `macos-11`
    * Bump Dependencies with SDL 2.0.16
    * Added .NET 6, removed .NET Core
    * Added integration with WinForms
    * Bump SDL 2.0.18
    * Bump ADO to `vs-2022`
    * Add overload `FromStream` to `Song` (Desktop/DX/WinRT)

---
+++ MonoGame.Framework/Platform/Windows/GameControl.cs
@@ -0,0 +1,42 @@
+﻿
+
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Microsoft.Xna.Framework.Windows
+{
+    /// <summary>
+    /// GameView to render game scene on Forms application
+    /// </summary>
+    [DesignerCategory("Code")]
+    internal class GameControl : Control
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public GameControl()
+        {
+            SetStyle(ControlStyles.Opaque, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint |
+                     ControlStyles.UserPaint,
+                     true);
+        }
+
+        /// <summary>
+        /// Paint event
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+        }
+
+
+        /// <summary>
+        /// Paint Background event
+        /// </summary>
+        /// <param name="pevent"></param>
+        protected override void OnPaintBackground(PaintEventArgs pevent)
+        {
+        }
+    }
+}
